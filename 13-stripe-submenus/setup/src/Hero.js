52530a31431d4730779dcfb3515b427bf1bcb358
veilvokay <romayakovlev@hotmail.com>
2021-06-10 23:53:23 +0300

    [WIP] Continue from 06:18:50

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,26 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+  const { closeSubmenu } = useGlobalContext();
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo
+            dignissimos alias ducimus adipisci veritatis quod.
+          </p>
+          <button className="btn">Start Now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} alt="phone" className="phone-img" />
+        </article>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
--- 13-stripe-submenus/setup/src/Navbar.js
+++ 13-stripe-submenus/setup/src/Navbar.js
@@ -1,9 +1,46 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
-}
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
 
-export default Navbar
+  const displaySubmenu = (e) => {
+    console.log("hi");
+    openSubmenu();
+  };
+
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" className="nav-logo" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              products
+            </button>
+          </li>
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              developers
+            </button>
+          </li>
+          <li>
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              company
+            </button>
+          </li>
+        </ul>
+        <button className="btn signin-btn">Sign In</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
